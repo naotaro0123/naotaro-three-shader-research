026c9dfb3d6fed67d7bc602bc6df4f8e089830c8
naotaro0123 <n.ikewada@gmail.com>
2020-07-24 16:39:54 +0900

    fix: add shader inline

--- src/Components/CustomizingVertexShader.ts
+++ src/Components/CustomizingVertexShader.ts
@@ -1,26 +1,54 @@
+// reference: https://blog.mozvr.com/customizing-vertex-shaders/
 import * as THREE from 'three';
 import { CommonThree } from './CommonThree';
-const vertexShader = require('../Shaders/customizingVertexShader.vert');
-const fragmentShader = require('../Shaders/customizingVertexShader.frag');
 
 export class CustomizingVertexShader extends CommonThree {
   private mesh: THREE.Mesh;
+  private materialShader: THREE.Shader;
+  private time = 0;
 
   constructor() {
     super({ isPerspective: true });
+    this.scene.background = new THREE.Color(0xcccccc);
   }
 
   addObjects() {
-    const geometry = new THREE.SphereGeometry(0.8, 32, 32);
-    const material = new THREE.ShaderMaterial({
-      vertexShader,
-      fragmentShader,
+    const geometry = new THREE.SphereGeometry(3.0, 32, 32);
+    const material = new THREE.MeshLambertMaterial({
+      color: 'green',
+      transparent: true,
+      opacity: 0.5,
     });
+    // onBeforeCompile equal rawShader
+    material.onBeforeCompile = (shader) => {
+      shader.uniforms['time'] = { value: 0 };
+      shader.vertexShader = `
+        uniform float time;
+      ` + shader.vertexShader;
+      const token = '#include <begin_vertex>';
+      const customTransform = `
+        vec3 transformed = vec3(position);
+        // transformed.x = position.x + position.y / 20.0;
+        transformed.x = position.x + sin(position.y * 10.0 + time + 10.0) * 0.1;
+      `;
+      shader.vertexShader = shader.vertexShader.replace(token, customTransform);
+      this.materialShader = shader;
+    }
     this.mesh = new THREE.Mesh(geometry, material);
     this.scene.add(this.mesh);
+
+    const light = new THREE.DirectionalLight(0xffffff, 1.0);
+    light.position.set(1, 1, 1).normalize();
+    this.scene.add(light);
+    this.scene.add(new THREE.AmbientLight(0xffffff, 0.3));
   }
 
   render() {
+    this.time++;
+    this.mesh.rotation.y += 0.01;
+    if (this.materialShader) {
+      this.materialShader.uniforms['time'].value = this.time / 1000;
+    }
     super.render();
   }
 }
